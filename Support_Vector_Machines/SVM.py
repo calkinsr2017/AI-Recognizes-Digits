9efdbba950c12f4a5d76cf9e036bbb7a314f3611
Robert Calkins <rcalkins@umd.edu>
2020-05-27 00:00:45 -0700

    finished KNN example and started SVM

---
+++ Support_Vector_Machines/SVM.py
@@ -0,0 +1,23 @@
+import sklearn
+from sklearn import datasets
+from sklearn import svm
+
+
+#######################
+#     Set up data     #
+#######################
+
+
+#Gunna load in datasets for this example
+
+cancer = datasets.load_breast_cancer() #loading a sklearn in house dataset! instead of downloading.
+
+print(cancer.feature_names)
+print(cancer.target_names)
+
+X = cancer.data 
+y = cancer.target
+
+x_train, x_test , y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.2) 
+
+classes = ['malignant', 'benign'] #0 and 1 from data will corrispond to this list. 
