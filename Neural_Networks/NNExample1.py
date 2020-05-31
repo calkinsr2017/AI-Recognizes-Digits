22d34e4df8cbd6844a533b256da02fad53d22a36
Robert Calkins <rcalkins@umd.edu>
2020-05-30 20:52:35 -0700

    edited readme

--- Neural_Networks/NNExample1.py
+++ Neural_Networks/NNExample1.py
@@ -1,8 +1,11 @@
-import tensorflow as tf 
-from tensorflow import keras
-import numpy as np 
-import matplotlib.pyplot as plt 
-import pickle 
+import warnings  
+with warnings.catch_warnings():  
+    warnings.filterwarnings("ignore",category=FutureWarning)
+    import tensorflow as tf 
+    from tensorflow import keras
+    import numpy as np 
+    import matplotlib.pyplot as plt 
+    import pickle
 
 #This is a simple example of a NN using the glothing dataset on keras. It is a picture based data. You can see what
 #The data looks like by uncommenting the plt plots below. 
@@ -13,15 +16,15 @@ RETRAIN = False
 #######################
 
 
-data = keras.datasets.fashion_mnist #built in dataset for different clothings
+data = keras.datasets.mnist #built in dataset for different clothings
 
 (train_images, train_labels), (test_images, test_labels) = data.load_data() #Gets the data in tuble form
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
-                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot']
+class_names = ['zero', 'one', 'two', 'three', 'four', 
+                'five', 'six', 'seven', 'eight', 'nine']
 
-#plt.imshow(train_images[7], cmap=plt.cm.binary) #shows the image
-#plt.show()
+plt.imshow(train_images[7], cmap=plt.cm.binary) #shows the image
+plt.show()
 
 train_images = train_images/255.0  #gets the greyscale value to be between 0 and 1. Its in a numpy array so this works
 test_images = test_images/255.0
@@ -51,7 +54,7 @@ model = loadModel(RETRAIN)
 
 prediction = model.predict(test_images)
 
-for i in range(5):
+for i in range(10, 15):
     specific = class_names[np.argmax(prediction[i])]
     plt.grid(False)
     plt.imshow(test_images[i], cmap = plt.cm.binary)
